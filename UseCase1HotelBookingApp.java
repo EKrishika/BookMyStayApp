13223a8671d6e342bbbfe6442211d02878d428cd
EKrishika <ekrishiks68@gmail.com>
2026-03-24 14:16:50 +0530

    Create UseCase1HotelBookingApp.java

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,22 @@
+/**
+ * Book My Stay Application - Entry Point
+ * This class represents the starting point of the Hotel Booking System.
+ * It demonstrates how a Java application begins execution.
+ *
+ * @author Shivam
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=======================================");
+    }
+}
